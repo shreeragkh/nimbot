8f4162c29ed6d3d35b5b09715baa8b4b70f7c46e
Jos Ephraim <josephraimofficial@gmail.com>
2025-02-27 09:13:40 +0530

    requirments issues fixed

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 import os
-from flask_cors import CORS
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
@@ -7,7 +6,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Set up Flask app
 app = Flask(__name__)
-CORS(app)
 
 
 # Load environment variables from .env
@@ -15,16 +13,16 @@ load_dotenv()
 
 
 # Initialize the ChatGoogleGenerativeAI model
-llm = ChatGoogleGenerativeAI(model="gemini-pro",timeout=1600)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", timeout=1600)
 
 
 def generate_response(prompt):
     try:
         print("generating response")
         response = llm.invoke(prompt)
-        print("response",response.content)
+        print("response", response.content)
     except Exception as e:
-        print('error occured',e)
+        print("error occured", e)
     # Convert the AIMessage object to a dictionary
     response_dict = {
         "content": response.content,
@@ -36,12 +34,11 @@ def generate_response(prompt):
 @app.route("/generate", methods=["POST"])
 def generate():
     data = request.json
-    print("data from frontend",data)
+    print("data from frontend", data)
     prompt = data.get("prompt")
     print(prompt)
     response = generate_response(prompt)
     return jsonify({"response": response})
-
 
 
 # Run the Flask app
